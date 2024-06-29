ef8c7966e3de77b39aa5344afb52201fbcf82805
ishan-26 <125141868+ishan-26@users.noreply.github.com>
2024-06-29 15:18:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ generation_config = {
 }
 ## Function to load Google Gemini Pro Vision API And get response
 
-def get_gemini_repsonse(input,image,prompt):
+def get_gemini_response(input,image,prompt):
   model = genai.GenerativeModel(model_name="gemini-1.5-pro",generation_config=generation_config,)     
   if image:
       response = model.generate_content([input, image[0], prompt])
