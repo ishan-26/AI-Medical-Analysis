3ff841117a13f9b6617a94dee6560300d88ed779
Ishan Dhotre <125141868+ishan-26@users.noreply.github.com>
2024-10-22 16:55:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,7 +42,6 @@ def input_image_setup(uploaded_file):
 
 st.set_page_config(page_title="Diagnosis App")
 st.header("Diagnosis App")
-input = st.text_input("Input Prompt: ", key="input")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 image = ""
